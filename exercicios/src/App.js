f028310ef4e78a6a96404950103012303163237a
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-07-07 16:12:06 -0300

    Passando parametro para componentes

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
-import CompPadrao, { Comp1, Comp2 } from './components/Multi'
-import Primeiro from './components/Primeiro'
+import MinMax from "./components/MinMax";
+//import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+//import Primeiro from './components/Primeiro'
 
 export default () => (
-  <View>
-      <CompPadrao />
+  <View style={style.App}>
+      <MinMax min="3" max="20" />
+      <MinMax min="1" max="94" />
+      {/*<CompPadrao />
       <Comp1 />
       <Comp2 />
-      <Primeiro />
+      <Primeiro />*/}
   </View>
 )
+
+const style = StyleSheet.create ({
+    App: {
+      flexGrow: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 20
+    }
+})
--- exercicios/src/components/Primeiro.js
+++ exercicios/src/components/Primeiro.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { Text } from 'react-native'
+import estilo from "./estilo";
 
-export default () => <Text>Hello World!</Text>
+export default () => <Text style={estilo.txtG}>Hello World!</Text>
