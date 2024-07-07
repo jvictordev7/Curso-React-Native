1b91ac3f4d0e9cf30eb8dc4238f4d8d83288f76d
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-07-06 23:59:06 -0300

    .

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View } from 'react-native'
 
-import { Comp1, Comp2 } from "./components/Multi";
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
 
 export default () => (
   <View>
+      <CompPadrao />
       <Comp1 />
       <Comp2 />
       <Primeiro />
--- exercicios/src/components/Multi.js
+++ exercicios/src/components/Multi.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Text } from 'react-native'
 
-function Comp() {
+function Comp ()  {
     return <Text>Comp #Oficial</Text>
 }
 
-export function Comp1() {
-    return <Text>Codigo 01</Text>
+function Comp1() {
+    return <Text>Comp 01</Text>
 }
 
-export function Comp2() {
-    return <Text>Codigo 02</Text>
+function Comp2() {
+    return <Text>Comp 02</Text>
 }
+
+export { Comp1, Comp2 }
+export default Comp
