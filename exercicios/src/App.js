837f0edd3bb5b306d0f5efc542edf376031b58fb
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-07-06 23:47:37 -0300

    Criei Multi.js

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View } from 'react-native'
 
+import { Comp1, Comp2 } from "./components/Multi";
 import Primeiro from './components/Primeiro'
 
 export default () => (
   <View>
+      <Comp1 />
+      <Comp2 />
       <Primeiro />
   </View>
 )
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from 'react-native'
+
+function Comp() {
+    return <Text>Comp #Oficial</Text>
+}
+
+export function Comp1() {
+    return <Text>Codigo 01</Text>
+}
+
+export function Comp2() {
+    return <Text>Codigo 02</Text>
+}
