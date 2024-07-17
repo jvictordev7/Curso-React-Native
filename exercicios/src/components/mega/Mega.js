6430639c237cc200a450da811157a372120dc80d
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-07-17 14:48:01 -0300

    Componente de Classe: Cuidado com o This

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput } from 'react-native';
 import Estilo from '../estilo';
 
 export default class Mega extends Component {
 
     state = {
-        qtdeNumero: this.props.qtdeNumero
+        qtdeNumeros: this.props.qtdeNumeros
     }
 
-    constructor(props) {
-        super(props)
+    alterrarQtdeNumero = (qtde) => {
+        this.setState({ qtdeNumeros: qtde })
     }
 
     render() {
         return (
-            <Text style={Estilo.txtG}>
-                Gerador de Mega-Sena
-                {this.state.qtdeNumero}
-            </Text>
+            <>
+                <Text style={Estilo.txtG}>
+                    Gerador de Mega-Sena
+                    {this.state.qtdeNumeros}
+                </Text>
+                <TextInput 
+                keyboardType={'numeric'} //TECLADO NUMERICO
+                style={{borderBottomWidth: 1}}
+                    placeholder='Qtde de Numeros'
+                    value={this.state.qtdeNumero}
+                    onChangeText={this.alterrarQtdeNumero }
+                />
+            </>
         )
     }
 }
